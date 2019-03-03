4766ed5fafb675a5290a795a2871117fa303f971
William Reese Sexton <reese.sexton01@gmail.com>
2019-03-03 13:12:54 -0500

    Got basic modifiers working

--- app/src/main/java/com/example/reese/dungeondorks/DiceRollerActivity.kt
+++ app/src/main/java/com/example/reese/dungeondorks/DiceRollerActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -17,22 +18,23 @@ class DiceRollerActivity : AppCompatActivity() {
         val btn = findViewById<Button>(R.id.btnRoll)
 
         btn.setOnClickListener {
-            rollDice(Integer(20), Integer(0))
+            rollDice(Integer.valueOf(20))
         }
     }
 
-    private fun rollDice(bound: Integer, bonus: Integer) {
+    private fun rollDice(bound: kotlin.Int) {
 
         val txt = findViewById<TextView>(R.id.txtRollResult)
+        val bonus = findViewById<EditText>(R.id.edtModifier).text
 
-        val diceResult = Random().nextInt(bound.toInt()) + 1
+        val diceResult = Random().nextInt(bound) + 1
 
         when {
             diceResult >= 20 -> txt.setTextColor(Color.GREEN)
             diceResult <= 1 -> txt.setTextColor(Color.RED)
             else -> txt.setTextColor(Color.BLACK)
         }
-        val strResult = "Rolled a " + ( diceResult + bonus.toInt() )
+        val strResult = "Rolled a " + ( diceResult + bonus.toString().toInt() )
 
         txt.text = strResult
     }
