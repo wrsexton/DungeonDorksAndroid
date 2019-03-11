5b090e4ebf5f6236f6adbedd4e8af86d950266c1
William Reese Sexton <reese.sexton01@gmail.com>
2019-03-11 18:06:28 -0400

    Base activity for spellbook added.

--- app/src/main/java/com/example/reese/dungeondorks/SpellBook.kt
+++ app/src/main/java/com/example/reese/dungeondorks/SpellBook.kt
@@ -2,11 +2,37 @@ package com.example.reese.dungeondorks
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.widget.TextView
+import android.widget.Toast
 
 class SpellBook : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spell_book)
+
+//        val spells = findViewById<TextView>(R.id.textView) //findViewById<RecyclerView>(R.id.lstSpells)
+//// ...
+//
+//// Instantiate the RequestQueue.
+//        val queue = Volley.newRequestQueue(this)
+//        val url = "http://dnd5eapi.co/api/spells/"
+//
+//// Request a string response from the provided URL.
+//        val stringRequest = StringRequest(
+//            Request.Method.GET, url,
+//            Response.Listener<String> { response ->
+//                // Display the first 500 characters of the response string.
+//                spells.text = "Response is: ${response.substring(0, 500)}"
+//            },
+//            Response.ErrorListener {
+//                spells.text = it.toString()
+//            })
+//
+//// Add the request to the RequestQueue.
+//        queue.add(stringRequest)
+
+
     }
 }
